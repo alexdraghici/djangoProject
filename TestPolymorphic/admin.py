0df2e50d7bcf1498ea2e022005d83ba30af7da09
alexd <alexdraghici84@yahoo.com>
2021-12-13 13:36:43 +0200

    small fix

--- TestPolymorphic/admin.py
+++ TestPolymorphic/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import BaseTest
-admin.site.register(BaseTest)
+from .models import BaseClass
+admin.site.register(BaseClass)
 # Register your models here.
